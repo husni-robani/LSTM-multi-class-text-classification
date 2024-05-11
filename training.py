0a2c41220717564729063fa71e578073090a7ea5
husni-robani <husnir2005@gmail.com>
2024-05-11 20:01:12 +0700

    add preprocessing data process

--- training.py
+++ training.py
@@ -1,4 +1,5 @@
 from core.data_setup import DataSetup
+from core.text_dataset import TextDataset
 from core import utils
 import numpy as np
 import pandas as pd
@@ -8,7 +9,7 @@ from gensim.models import Word2Vec
 
 try: 
     df = pd.read_csv("data/abstract_dataset.csv")
-    w2v_model = Word2Vec.load("core/w2v_model.w2v")
+    w2v_model = Word2Vec.load("core/w2v_model/w2v_model.w2v")
 except FileNotFoundError as e:
     raise f"FileError: {e}"
 
@@ -18,19 +19,17 @@ X_train, X_test, y_train, y_test = train_test_split(df['abstract'].values, df['s
 # Preprocessing Data
 datasetup = DataSetup(X_train=X_train, X_test=X_test, y_train=y_train, y_test=y_test, w2v_model=w2v_model, is_oversampling=True)
 try:
-    print("=============PREPROCESSING=============")
+    print("=============PREPROCESSING START=============")
     X_train, X_test, y_train, y_test = datasetup.processing_data()
 except Exception as e:
     raise "Error on preprocessing"
 else:
+    print('\n')
     print("=============PREPROCESSING DONE=============")
 
 # Dataset
 
+
 # DataLoader
 
 # training loop
-
-
-
-print(X_train.shape, X_test.shape)
